2d3ca57fbde3bd7089c4055820e2a40cad38bcba
krasilovalex <onexoffalex@gmail.com>
2026-07-03 18:38:44 +0300

    feat: load configuration in application entrypoints

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,22 +2,41 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/krasilovalex/pulsewarden/internal/platform/config"
 	"github.com/krasilovalex/pulsewarden/internal/platform/lifecycle"
 )
 
 func main() {
+	os.Exit(run())
+}
+
+func run() int {
+
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "load config: %v\n", err)
+		return 1
+	}
 	logger := log.New(os.Stdout, "api: ", log.LstdFlags|log.LUTC|log.Lmsgprefix)
 
 	ctx, stop := lifecycle.SignalContext(context.Background())
 	defer stop()
 
-	logger.Println("started")
+	logger.Printf(
+		"started environment=%s log_level=%s shutdown_timeout=%s",
+		cfg.Environment,
+		cfg.LogLevel,
+		cfg.ShutdownTimeout,
+	)
 
 	<-ctx.Done()
 
 	logger.Println("shutdown requested")
 	logger.Println("stopped")
+
+	return 0
 }
